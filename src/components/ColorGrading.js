8a0484958884d848d9a6cbdb613a45e1c43fe965
matteopelusi7 <pelusimatteo02@gmail.com>
2022-11-28 13:20:18 +0100

    completed

--- src/components/ColorGrading.js
+++ src/components/ColorGrading.js
@@ -5,16 +5,38 @@ import { v4 as uuidv4 } from "uuid";
 
 const ColorGrading = () => {
 
+    const [isError, setIsError] = useState(false)
+
+    const [selectedColor, setSelectedColor] = useState([])
+
     const [colorInput, setColorInput] = useState({
         color: '',
         qty: 5
     })
+
  
     const handleSubmit = (e) => {
         e.preventDefault();
+        if(colorInput.color && colorInput.qty) {
+            const {color, qty} = colorInput
+            try {
+                setSelectedColor(
+                    new Values(color).all(Math.round(100/ parseInt(qty, 10) * 2))
+                );
+                setColorInput({
+                    color: '',
+                    qty: 10
+                })
+            } catch (error) {
+                setIsError(true)
+            }
+        }
     }
 
     const handleChange = (e) => {
+        if(isError) {
+            setIsError(false);
+        }
         const {name, value} = e.target;
 
         setColorInput({
@@ -22,6 +44,11 @@ const ColorGrading = () => {
             [name]: value,
         })
     }
+
+    useEffect(() => {
+        setColorInput({qty: 10, color: '#1194ec'})
+        setSelectedColor(new Values('#1194ec').all(Math.round(100/ 10) * 2))
+    }, []);
 
     return (
         <>
@@ -32,6 +59,15 @@ const ColorGrading = () => {
                 </div>
                 <button className="btn btn-selector" type="submit">Create</button>
             </form>
+            <section className="color-section">
+                {isError ? (
+                <h4 className="section-center">Nessun Colore Trovato</h4>
+                ) : !isError && selectedColor ? (
+                selectedColor.map((el) => <SingleColor key={uuidv4()} {...el} />)
+                ) : (
+                <h4>Loading</h4>
+                )}
+            </section>
         </>
     );
 };
